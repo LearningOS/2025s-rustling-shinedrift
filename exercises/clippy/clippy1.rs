e89e404f2cf37c4ea048709651331f9b258d9a32
drift <2897756320@qq.com>
2025-04-01 02:40:47 -0700

    test

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -11,13 +11,13 @@
 
 
 
-use std:: f64;
+use std:: f32;
 
 fn main() {
-    let pi = f64::consts::PI;
-    let radius = 5.00f64;
+    let pi = f32::consts::PI;
+    let radius = 5.00f32;
 
-    let area = pi * f64::powi(radius, 2);
+    let area = pi * f32::powi(radius, 2);
 
     println!(
         "The area of a circle with radius {:.2} is {:.5}!",
